450d5d083c857f1c8fd9cdb358532c9b936b5087
eejdoowad <eejdoowad@gmail.com>
2015-03-01 00:29:46 -0500

    adding files

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "chip8.h"
+#include "c8_util.h"
 
 int main(int argc, const char** argv)
 {
 	printf("\n\n\nChip8r\n\n\n\n");
+
+	struct chip8 c;
+
+	c8_print_mem(c.mem, 0, 200);
+
+
 	return 0;
 }
