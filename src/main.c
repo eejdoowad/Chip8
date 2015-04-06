f4ae1e06404ae36f24edb4e56cf90bdf37970c1c
eejdoowad <eejdoowad@gmail.com>
2015-04-06 02:03:18 -0400

    Improved Online Version

--- src/main.c
+++ src/main.c
@@ -14,6 +14,9 @@
 static CPU cpu;
 static IO_Module io;
 static bool quit;
+static char ROM[512];
+static const char* DEFAULT_ROM = "PONG";
+
 
 void one_cycle(void)
 {
@@ -26,6 +29,35 @@ void one_cycle(void)
 	io_updateKeys(&cpu, &io, &quit);
 }
 
+void set_ROM(char const * const new_rom)
+{
+	fprintf(stderr, "ROM CHANGED\n");
+	if (strlen(ROM) < 512)
+	{
+		strcpy(ROM, new_rom);
+	}
+	else
+	{
+		fprintf(stderr, "ROM file name length exceeded maximum of 512\nLoading %s instead", DEFAULT_ROM);
+		strcpy(ROM, DEFAULT_ROM);
+	}
+}
+
+// Emscripten specific functions
+#ifdef __EMSCRIPTEN__
+void change_ROM_emscripten(char const * const new_rom)
+{
+	emscripten_pause_main_loop();
+	
+	// Reset CPU state and load in new ROM
+	cpu_initialize(&cpu);
+	set_ROM(new_rom); // sets ROM and checks for errors
+	cpu_loadROM(&cpu, ROM); // S* Use return value later for error checking
+
+	emscripten_resume_main_loop();
+}
+#endif // __EMSCRIPTEN__
+
 int main(int argc, char* argv[])
 {
 	cpu_initialize(&cpu);
@@ -37,20 +69,24 @@ int main(int argc, char* argv[])
 	}
 
 	quit = false;
-	
-	char * ROM_TO_LOAD = "PONG";
+
+	// Select and load Startup ROM
 	if (argc >= 2)
 	{
-		ROM_TO_LOAD = argv[1];
+		strcpy(ROM, argv[1]);
+	}
+	else
+	{
+		set_ROM(DEFAULT_ROM);
 	}
 
-	if (cpu_loadROM(&cpu, ROM_TO_LOAD) == 1)
+	if (cpu_loadROM(&cpu, ROM) == 1)
 	{
 		fprintf(stderr, "Loading Rom Failed\n");
 		return 1;
 	}
 
-	io_updateWindowTitle(ROM_TO_LOAD, &io);
+	io_updateWindowTitle(ROM, &io);
 
 #ifdef __EMSCRIPTEN__
 	emscripten_set_main_loop(one_cycle, CYCLES_PER_SECOND, 1);
